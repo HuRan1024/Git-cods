044a74c3b245a1cb33ab985d87069d0527c90d43
HuRan1024 <2821650791@qq.com>
2023-10-20 19:28:21 +0800

    update

--- homework1/extra/3.cpp
+++ homework1/extra/3.cpp
@@ -7,51 +7,36 @@
 #define START 48
 
 int main() {
-    char a[1001], b[1001], c[1001] = {0};
-    char s[2002][1001];
-    int n = 0;
-    while (scanf("%s", s[n]) != EOF) {
-        n++;        //读入不定行，二维数组s中每一行为一个数
-    }
-    for (int p = 0; p <= n; p++) {                   //将两行的s分别赋给a,b；然后进行加算
-        for (int i = 0; i < 1001; i++) {
-            a[i] = s[p][i];
-        }
-        p++;
-        for (int i = 0; i < 1001; i++) {
-            b[i] = s[p][i];
-        }
+    char a[1010], b[1010], c[1010] = {0};
+    while(scanf("%s",a) != EOF && scanf("%s",b) != EOF){                //将两行的s分别赋给a,b；然后进行加算
         int n1 = strlen(a), n2 = strlen(b);
         //printf ("%d %d", n1, n2);
         int carry = 0, k = 0;
         int i, j;
         for (i = n1 - 1, j = n2 - 1; i >= 0 && j >= 0; i--, j--) {
-            c[k] = (a[i] - START + b[j] - START) % 10 + carry + START;
-            carry = (a[i] - START + b[j] - START) / 10;
+            c[k] = (a[i] - START + b[j] - START + carry)  % 10 + START;
+            carry = (a[i] - START + b[j] - START + carry) / 10;
             k = k + 1;
         }               //先加算所有能加的位数
         if (i == -1 && j == -1) {
-            if (carry == 1) {
-                c[k] = 49;
-                k++;
-            }
+            ;
         } else if (i == -1) {
-            c[k] = b[j] + carry;
-            j--;
-            k++;
-            for (; j > 0; j--) {
-                c[k] = b[j];
+            for (; j >= 0; j--) {
+                c[k] = (b[j] + carry - START) % 10 + START;
+                carry = (b[j] - START + carry) / 10;
                 k++;
             }
         } else if (j == -1) {
-            c[k] = a[i] + carry;
-            i--;
-            k++;
-            for (; i > 0; i--) {
-                c[k] = a[i];
+            for (; i >= 0; i--) {
+                c[k] = (a[i] + carry - START) % 10 +START;
+                carry = (a[i] + carry - START) / 10;
                 k++;
             }
-        }                 //再读取剩下的未加减的位数；
+        }            //再读取剩下的未加减的位数；
+        if (carry == 1) {
+            c[k] = 49;
+            k++;
+        }
         for (k = k - 1; k >= 0; k--) {
             printf("%c", c[k]);
         }
--- prctice/try.cpp
+++ prctice/try.cpp
@@ -1,55 +1,78 @@
-//
-// Created by reprise on 2023/10/6.
-//
-#include<stdio.h>
-#include <math.h>
-#include<string.h>
-#include<stdlib.h>
-int main()
-{
-    /*int ch=1;
-    while((ch=getchar())!=EOF)
-    {
-        putchar(ch);
-    }
-    printf("EDF");*/
-    /*int a[2]={1};
-    for(int i=0;i<2;i++)
-    {
-        if(a[i]=='\0') printf("6");
-    }*/
-    /*int a[10],i;
-    for(i=0;i<10;i++)
-    {
-        printf("%d ",a[i]);
-    }*/
-    /*long long i = 0;
-    for ( i = 0; i >= 0; ){
-        i++;
-    }
-    printf("%ld" ,i);*/
-//    int n;
-//    scanf("%d",&n);
-//    int a[n];
-//    for (int i = 0; i < n; i++){
-//        scanf("%d",&a[i]);
+////
+//// Created by reprise on 2023/10/6.
+////
+//#include<stdio.h>
+//#include <math.h>
+//#include<string.h>
+//#include<stdlib.h>
+//int main()
+//{
+//    /*int ch=1;
+//    while((ch=getchar())!=EOF)
+//    {
+//        putchar(ch);
 //    }
-//    for (int i = 0; i < n; i++) {
-//        for (int j = 0; j < n - i - 2; j++) {
-//            if (a[j] > a[j + 1]) {
-//                int tmp = a[j];
-//                a[j] = a[j + 1];
-//                a[j + 1] = tmp;
-//            }
-//        }
-//    }
-//    for (int i = 0; i < n; i++){
+//    printf("EDF");*/
+//    /*int a[2]={1};
+//    for(int i=0;i<2;i++)
+//    {
+//        if(a[i]=='\0') printf("6");
+//    }*/
+//    /*int a[10],i;
+//    for(i=0;i<10;i++)
+//    {
 //        printf("%d ",a[i]);
+//    }*/
+//    /*long long i = 0;
+//    for ( i = 0; i >= 0; ){
+//        i++;
 //    }
-   /* char a[10];
-    scanf("%s",a);
-    int i = atoi(a);
-    printf("%d",i);*/
+//    printf("%ld" ,i);*/
+////    int n;
+////    scanf("%d",&n);
+////    int a[n];
+////    for (int i = 0; i < n; i++){
+////        scanf("%d",&a[i]);
+////    }
+////    for (int i = 0; i < n; i++) {
+////        for (int j = 0; j < n - i - 2; j++) {
+////            if (a[j] > a[j + 1]) {
+////                int tmp = a[j];
+////                a[j] = a[j + 1];
+////                a[j + 1] = tmp;
+////            }
+////        }
+////    }
+////    for (int i = 0; i < n; i++){
+////        printf("%d ",a[i]);
+////    }
+//   /* char a[10];
+//    scanf("%s",a);
+//    int i = atoi(a);
+//    printf("%d",i);*/
+//
+//    return 0;
+//
+//}
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int x,i = 1,sum = 0,k;
+    for (x = 100000; x < 1000000; x++)
+    {
+        //x = 25;
+        if (x == (int)sqrt(x) * (int)sqrt(x)){
+            //printf("YES");
+            k = x;
+            while ( k > 0){
+                sum = sum * 10 + k % 10;
+                k /= 10;
+            }
+            if (sum == x){
+                printf("%d\n",x);
+            }
+        }
+    }
     return 0;
-
 }
